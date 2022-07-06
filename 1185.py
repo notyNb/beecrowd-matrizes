2b450da574166f5bd887a0a202088f815d213da7
Pedro Lucas <100409572+notyNb@users.noreply.github.com>
2022-07-05 22:56:23 -0400

    Create 1185.py

---
+++ 1185.py
@@ -0,0 +1,45 @@
+def soma(m: list):
+    resultado = 0.0
+    p = 12
+    for i in range(11):
+        p -= 1
+        for j in range(0, p):
+            resultado += m[i][j]
+            
+    print(f"{resultado:.1f}")
+
+def media(m: list):
+    resultado = 0
+    p = 12
+    for i in range(11):
+        p -= 1
+        for j in range(0, p):
+            resultado += m[i][j]
+    media_final = resultado / (66)
+    print(f"{media_final:.1f}")
+
+
+
+
+def main():
+    m = []
+    a = 12
+    b = 12
+    for i in range(a):
+        linha = [0] * b
+        m.append(linha)
+        
+    somaMedia = input()
+    for i in range(12):
+        for j in range(12):
+            valores = float(input())
+            m[i][j] = valores
+
+       
+    if somaMedia == "S":
+        soma(m)
+    elif somaMedia =="M":
+        media(m)
+
+        
+main()
